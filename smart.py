99dd1b898ef53051a8786cbeb0c977e090a63ea1
PMayala <p.mayala@alustudent.com>
2023-11-25 02:40:22 -0800

    smart.py

--- smart.py
+++ smart.py
@@ -4,11 +4,7 @@ class User:
     def __init__(self, name):
         self.name = name
         self.progress = {"Educational Modules": 0, "Q&A Sessions": 0, "Job Applications": 0}
-     
-     
-     
-     
-         def __init__(self):
+
 
          def introduction(self):
              print("Welcome to the Smart Farming Platform!")
