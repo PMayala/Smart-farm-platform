98b41d494cfc3bd5ee5fe79b560157c9262b6c3d
Khaalid245 <k.abdillahi@alustudent.com>
2023-11-25 02:32:46 -0800

    smart.py

--- smart.py
+++ smart.py
@@ -4,3 +4,13 @@ class User:
     def __init__(self, name):
         self.name = name
         self.progress = {"Educational Modules": 0, "Q&A Sessions": 0, "Job Applications": 0}
+     
+     
+     
+     
+         def __init__(self):
+
+         def introduction(self):
+             print("Welcome to the Smart Farming Platform!")
+             print("Our mission is to empower farmers through education, collaboration, and employment opportunities.")
+            print("Let's embark on this journey together!\n")
