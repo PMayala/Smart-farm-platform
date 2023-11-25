d59797cda993aba3d2b9530dc10120bcdedaf0e9
PMayala <p.mayala@alustudent.com>
2023-11-25 04:43:35 -0800

    hh

--- smart.py
+++ smart.py
@@ -11,10 +11,9 @@ class SmartFarmingPlatform:
         self.notifications = []
 
     def introduction(self):
-             print("Welcome to the Smart Farming Platform!")
-             print("Our mission is to empower farmers through education, collaboration, and employment opportunities.")
-             print("Let's embark on this journey together!\n")
-
+        print("Welcome to the Smart Farming Platform!")
+        print("Our mission is to empower farmers through education, collaboration, and employment opportunities.")
+        print("Let's embark on this journey together!\n")
 
     def start_prompt(self):
         while True:
@@ -38,13 +37,11 @@ class SmartFarmingPlatform:
         print("5. Notifications")
         print("6. Exit")
 
-
     def educational_module(self, user):
         print("\n=== Educational Modules ===")
         print("1. Modern Farming Techniques")
         print("2. Sustainable Practices")
         choice = input("Enter the number of the module you want to explore: ")
-
 
         if choice == "1":
             print("\n=== Modern Farming Techniques ===")
@@ -66,18 +63,60 @@ class SmartFarmingPlatform:
         question = input("Ask a question to the agricultural experts: ")
         print(f"Thank you for your question, {user.name}! Our experts will respond shortly.")
         user.progress["Q&A Sessions"] += 1
-         
+
     def job_portal(self, user):
         print("\n=== Job Portal ===")
         print("1. Agricultural Specialist - Company A")
         print("2. Farm Manager - Company B")
         choice = input("Enter the number of the job you want to apply for: ")
+
         if choice in ["1", "2"]:
             print(f"Congratulations, {user.name}! You've applied for a job. Good luck!")
             user.progress["Job Applications"] += 1
         else:
             print("Invalid choice. Please enter a valid job number.")
 
-    if __name__ == "__main__":
+    def check_progress(self, user):
+        print("\n=== Progress Tracking ===")
+        print(f"Hello {user.name}! Your progress so far:")
+        for category, progress in user.progress.items():
+            print(f"{category}: {progress}")
+
+    def display_notifications(self):
+        print("\n=== Notifications ===")
+        for notification in self.notifications:
+            print(notification)
+
+    def main(self):
+        self.introduction()
+        self.start_prompt()
+
+        while True:
+            username = input("Enter your name: ")
+            if username not in self.users:
+                self.users[username] = User(username)
+
+            user = self.users[username]
+
+            self.display_dashboard()
+            choice = input("\nEnter the number of your choice: ")
+
+            if choice == "1":
+                self.educational_module(user)
+            elif choice == "2":
+                self.qna_interaction(user)
+            elif choice == "3":
+                self.job_portal(user)
+            elif choice == "4":
+                self.check_progress(user)
+            elif choice == "5":
+                self.display_notifications()
+            elif choice == "6":
+                print(f"Thank you, {user.name}, for exploring Smart Farming. Have a great day!")
+                break
+            else:
+                print("Invalid choice. Please enter a number from 1 to 6.\n")
+
+if __name__ == "__main__":
     platform = SmartFarmingPlatform()
     platform.main()
