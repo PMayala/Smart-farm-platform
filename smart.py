cc9b26210287a6afb79ed1f321132f4c198dfa1d
yusufmolumo <y.molumo@alustudent.com>
2023-12-07 10:59:11 -0800

    updated

--- smart.py
+++ smart.py
@@ -4,3 +4,20 @@ class User:
        	self.user_id = user_id
        	self.name = name
        	self.progress = {"Educational Modules": 0, "Q&A Sessions": 0, "Job Applications": 0}
+
+class SmartFarmingPlatform:
+    def __init__(self):
+       	self.users = {}
+       	self.notifications = []
+       	self.db_connection = sqlite3.connect('smart_farming.db')
+
+    def create_tables(self):
+   	with self.db_connection:
+       	cursor = self.db_connection.cursor()
+       	cursor.execute('''
+           	CREATE TABLE IF NOT EXISTS users (
+               	id INTEGER PRIMARY KEY AUTOINCREMENT,
+               	username TEXT NOT NULL,
+               	password TEXT NOT NULL
+           	)
+       	''')
